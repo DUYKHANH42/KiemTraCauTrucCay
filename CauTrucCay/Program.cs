d1ddf3021f2831772f73c8cdbc8cdfc61a0e6b0a
DUYKHANH42 <kxtduykhanh@gmail.com>
2025-03-31 16:01:57 +0700

    ThemXoaCay()

--- CauTrucCay/Program.cs
+++ CauTrucCay/Program.cs
@@ -72,7 +72,18 @@ namespace CauTrucCay
                 ThemNut(ref this.root, x);
             }
         }
+        public void XoaCay()
+        {
+            if (root == null)
+            {
+                Console.WriteLine("Cây rỗng");
+            }
+            else
+            {
+                Console.WriteLine("Xóa cây thành công");
 
+            }
+        }
         public void LNR(TNode root)  //duyet cay theo thu tu giua
         {
             if (root != null)
@@ -100,7 +111,7 @@ namespace CauTrucCay
             }
             if (root.left == null && root.right == null)
             {
-                return root.data;
+                return 1 + DemSoNutLa(root.left) + DemSoNutLa(root.right);
             }
             return DemSoNutLa(root.left) + DemSoNutLa(root.right);
         }
@@ -122,85 +133,106 @@ namespace CauTrucCay
             }
             return root.data + TinhTong(root.left) + TinhTong(root.right);
         }
-    }
-
-
-    //cau 2
-    class Program
-    {
-        static void Main(string[] args)
+        public void XoaCay(ref TNode root)
         {
-            MeNu.Menu();
-            Console.ReadKey();
-        }
-    }
-    class MeNu
-    {
-        public static void clear()
-        {
-            Console.Clear();
-        }
-        public static void Menu()
-        {
-            Console.OutputEncoding = Encoding.UTF8;
-            BST tree = new BST();
-            int chon;
-            do
+            if (root != null)
             {
-                Console.WriteLine("Chương trình quản lý cây nhị phân tìm kiếm");
-                Console.WriteLine("1. Tạo Cây");
-                Console.WriteLine("2. Thêm Nút");
-                Console.WriteLine("3. Duyệt cây theo LNR");
-                Console.WriteLine("4. Đếm số nút của cây");
-                Console.WriteLine("5. Đếm số nút lá của cây");
-                Console.WriteLine("6. Tìm phần tử nhỏ nhất của cây");
-                Console.WriteLine("7. Tính tổng các phần tử trong cây");
-                Console.WriteLine("8.Thoát");
-                chon = int.Parse(Console.ReadLine());
-                switch (chon)
-                {
-                    case 1:
-                        clear();
-                        tree.TaoCay();
-                        break;
-                    case 2:
-                        clear();
-                        Console.WriteLine("Nhập Phần Tử Cần Thêm: ");
-                        int x = int.Parse(Console.ReadLine());
-                        tree.ThemNut(ref tree.root, x);
-                        break;
-                    case 3:
-                        clear();
-                        Console.WriteLine("Duyệt Cây Theo LNR: ");
-                        tree.LNR(tree.root);
-                        break;
-                    case 4:
-                        clear();
-                        Console.WriteLine("Số Nút Của Cây: {0}", tree.DemSoNut(tree.root));
-                        break;
-                    case 5:
-                        clear();
-                        Console.WriteLine("Số Nút Lá Của Cây: {0}", tree.DemSoNutLa(tree.root));
-                        break;
-                    case 6:
-                        clear();
-                        Console.WriteLine("Phần Tử Nhỏ Nhất Của Cây: {0}", tree.TimMin(tree.root));
-                        break;
-                    case 7:
-                        clear();
-                        Console.WriteLine("Tổng Các Phần Tử Trong Cây: {0}", tree.TinhTong(tree.root));
-                        break;
-                    case 8:
-                        clear();
-                        Console.WriteLine("Hẹn Gặp Lại!");
-                        break;
-                    default:
-                        clear();
-                        Console.WriteLine("Chức năng không tồn tại");
-                        break;
-                }
+                XoaCay(ref root.left);
+                XoaCay(ref root.right);
+                root = null;
+                Console.WriteLine("Xóa cây thành công");
             }
-            while (chon != 8);
+            else
+            {
+                Console.WriteLine("Cây rỗng");
+            }
+        }
+
+
+
+        //cau 2
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                MeNu.Menu();
+                Console.ReadKey();
+            }
+        }
+        class MeNu
+        {
+            public static void clear()
+            {
+                Console.Clear();
+            }
+            public static void Menu()
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+                BST tree = new BST();
+                int chon;
+                do
+                {
+                    Console.WriteLine("Chương trình quản lý cây nhị phân tìm kiếm");
+                    Console.WriteLine("1. Tạo Cây");
+                    Console.WriteLine("2. Thêm Nút");
+                    Console.WriteLine("3. Duyệt cây theo LNR");
+                    Console.WriteLine("4. Đếm số nút của cây");
+                    Console.WriteLine("5. Đếm số nút lá của cây");
+                    Console.WriteLine("6. Tìm phần tử nhỏ nhất của cây");
+                    Console.WriteLine("7. Tính tổng các phần tử trong cây");
+                    Console.WriteLine("8. Xóa cây");
+                    Console.WriteLine("9.Thoát");
+                    chon = int.Parse(Console.ReadLine());
+                    switch (chon)
+                    {
+                        case 1:
+                            clear();
+                            tree.TaoCay();
+                            break;
+                        case 2:
+                            clear();
+                            Console.WriteLine("Nhập Phần Tử Cần Thêm: ");
+                            int x = int.Parse(Console.ReadLine());
+                            tree.ThemNut(ref tree.root, x);
+                            break;
+                        case 3:
+                            clear();
+                            Console.WriteLine("Duyệt Cây Theo LNR: ");
+                            tree.LNR(tree.root);
+                            Console.WriteLine();
+                            break;
+                        case 4:
+                            clear();
+                            Console.WriteLine("Số Nút Của Cây: {0}", tree.DemSoNut(tree.root));
+                            break;
+                        case 5:
+                            clear();
+                            Console.WriteLine("Số Nút Lá Của Cây: {0}", tree.DemSoNutLa(tree.root));
+                            break;
+                        case 6:
+                            clear();
+                            Console.WriteLine("Phần Tử Nhỏ Nhất Của Cây: {0}", tree.TimMin(tree.root));
+                            break;
+                        case 7:
+                            clear();
+                            Console.WriteLine("Tổng Các Phần Tử Trong Cây: {0}", tree.TinhTong(tree.root));
+                            break;
+                        case 8:
+                            clear();
+                            tree.XoaCay(ref tree.root);
+                            break;
+                        case 9:
+                            clear();
+                            Console.WriteLine("Hẹn Gặp Lại!");
+                            break;
+                        default:
+                            clear();
+                            Console.WriteLine("Chức năng không tồn tại");
+                            break;
+                    }
+                }
+                while (chon != 9);
+            }
         }
     }
 }
